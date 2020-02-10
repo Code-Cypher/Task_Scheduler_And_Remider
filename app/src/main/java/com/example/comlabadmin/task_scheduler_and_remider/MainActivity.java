7986fe3fdb598d2530038edd59bb7ebeb90964b1
COMLABADMIN <jkings.zero@gmail.com>
2020-02-10 09:53:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/comlabadmin/task_scheduler_and_remider/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.comlabadmin.task_scheduler_and_remider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
