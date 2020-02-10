d903f70b492c0c8a1b780b5d65424e3d5b57d3e5
COMLABADMIN <jkings.zero@gmail.com>
2020-02-10 12:44:26 +0800

    2nd

--- app/src/main/java/com/example/comlabadmin/task_scheduler_and_remider/Pop.java
+++ app/src/main/java/com/example/comlabadmin/task_scheduler_and_remider/Pop.java
@@ -1,13 +1,24 @@
 package com.example.comlabadmin.task_scheduler_and_remider;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.util.DisplayMetrics;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import java.util.Calendar;
 /**
  * Created by COMLABADMIN on 10/02/2020.
  */
 public class Pop extends Activity{
+
+    EditText date, time;
+    Button btndate;
+    private int mYear, mMonth, mDay, nMins;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,28 @@ public class Pop extends Activity{
 
         getWindow().setLayout((int)(width*.8), (int)(height*.8));
 
+        btndate = (Button) findViewById(R.id.btnDate);
 
+        date = (EditText) findViewById(R.id.etDate);
+
+        /*btndate.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                mYear = calendar.get(Calendar.YEAR);
+                mMonth = calendar.get(Calendar.MONTH);
+                mDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dateTimePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        date.setText(dayOfMonth + " " + month + " " + year);
+
+                    }
+                },mYear,mMonth,mDay);
+                dateTimePicker.show();
+            }
+        });*/
     }
 }
